2fd6cacdf3d1a2c52c4def9dd3c10fdcb95c942b
Dudi Varaprasad <94173498+bezalel-7@users.noreply.github.com>
2023-12-09 05:19:54 +0000

    added kubectl installation in aws

--- Installation/eks/ubuntu-eks-setup.sh
+++ Installation/eks/ubuntu-eks-setup.sh
@@ -3,3 +3,10 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
+#kubectl 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl version
+kubectl version --short
+
