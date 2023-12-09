0d08eb6bccff7b2a647f5d2a1a9e24d8ada8f048
Dudi Varaprasad <94173498+bezalel-7@users.noreply.github.com>
2023-12-09 13:22:38 +0530

    Update ubuntu-eks-setup.sh
    added helm config for ubuntu

--- Installation/eks/ubuntu-eks-setup.sh
+++ Installation/eks/ubuntu-eks-setup.sh
@@ -13,5 +13,9 @@ kubectl version --short
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+#helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
 
