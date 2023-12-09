c177352a736274aa016a596f618570a61ed53cb4
Dudi Varaprasad <94173498+bezalel-7@users.noreply.github.com>
2023-12-09 13:19:38 +0530

    Update ubuntu-eks-setup.sh
    modified eksctl script

--- Installation/eks/ubuntu-eks-setup.sh
+++ Installation/eks/ubuntu-eks-setup.sh
@@ -10,9 +10,8 @@ sudo mv ./kubectl /usr/local/bin
 kubectl version
 kubectl version --short
 #eksctl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin
-kubectl version
-kubectl version --short
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
 
